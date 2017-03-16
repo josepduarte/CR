a5225e8eb75d0fb4041f95f91e2a49f14b45d04f
José Pedro Duarte <josepduarte@ua.pt>
2017-03-16 07:56:11 +0000

    initial stuff from aula2

---
+++ Aula3/ex2/ex2.srcs/sources_1/new/Greatest_common_divisor.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/16/2017 07:54:34 AM
+-- Design Name: 
+-- Module Name: Greatest_common_divisor - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Greatest_common_divisor is
+--  Port ( );
+end Greatest_common_divisor;
+
+architecture Behavioral of Greatest_common_divisor is
+
+begin
+
+
+end Behavioral;
